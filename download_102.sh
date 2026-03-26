421513aaf39c5ea4fabac39640b341bb6cda10bc
Jarl-Penguin <jarlpenguin@outlook.com>
2026-03-27 02:36:25 +0300

    Add 102 form dbf files and script
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>

---
+++ download_102.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+for ((i=2004; i<=2025; i++)); do
+    for ((j=1; j<=10; j+=3)); do
+        if [ $j -lt 10 ]; then
+            k=0"$j"
+        elif [ $j == 13 ]; then
+            k=01
+        else
+            k=$j
+        fi
+        if [ $i -lt 2009 ]; then
+            ext="zip"
+        else
+            ext="rar"
+        fi
+        filename=https://www.cbr.ru/vfs/credit/forms/102-"${i}${k}"01."${ext}"
+        wget "$filename" -O file.rar
+        if [ $i -lt 2009 ]; then
+            unzip -o file.rar *200*_P* -d data
+        else
+            unar -D -f file.rar *_P1* -o data
+        fi
+    done
+done
+filename=https://www.cbr.ru/vfs/credit/forms/102-20260101.rar
+wget "$filename" -O file.rar
+unar -D -f file.rar *_P1* -o data
+rm file.rar
