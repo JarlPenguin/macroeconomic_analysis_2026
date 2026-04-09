c448d0bad0367cda23040c71c660202b06960c2b
Jarl-Penguin <jarlpenguin@outlook.com>
2026-04-10 00:19:27 +0300

    Add missing 102 form dbf files and update script
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>

--- download_102.sh
+++ download_102.sh
@@ -17,13 +17,15 @@ for ((i=2004; i<=2025; i++)); do
         filename=https://www.cbr.ru/vfs/credit/forms/102-"${i}${k}"01."${ext}"
         wget "$filename" -O file.rar
         if [ $i -lt 2009 ]; then
-            unzip -o file.rar *200*_P* -d data
+            unzip -o file.rar *_P*.* -d dbf
+            unzip -p file.rar *SPRAV1* > dbf/SPRAV1_"${k}${i}".dbf
         else
-            unar -D -f file.rar *_P1* -o data
+            unar -D -f file.rar *_P*.* -o dbf
+            unar -D -f file.rar -o - *SPRAV1* > dbf/SPRAV1_"${k}${i}".dbf
         fi
     done
 done
 filename=https://www.cbr.ru/vfs/credit/forms/102-20260101.rar
 wget "$filename" -O file.rar
-unar -D -f file.rar *_P1* -o data
+unar -D -f file.rar *_P*.* -o data
 rm file.rar
