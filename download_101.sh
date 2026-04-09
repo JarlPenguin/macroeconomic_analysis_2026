c89ff1870bd59edfaea2d3c533c7f546482e42c6
Jarl-Penguin <jarlpenguin@outlook.com>
2026-04-10 00:06:01 +0300

    Add missing 101 form dbf files and update script
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>

--- download_101.sh
+++ download_101.sh
@@ -17,13 +17,15 @@ for ((i=2004; i<=2025; i++)); do
         filename=https://www.cbr.ru/vfs/credit/forms/101-"${i}${k}"01."${ext}"
         wget "$filename" -O file.rar
         if [ $i -lt 2009 ]; then
-            unzip -o file.rar *200*_B* -d data
+            unzip -o file.rar *B*.* -d dbf
+            unzip -p file.rar *NAMES* > dbf/NAMES_"${k}${i}".dbf
         else
-            unar -D -f file.rar *B1* -o data
+            unar -D -f file.rar *B*.* -o dbf
+            unar -D -f file.rar -o - *NAMES* > dbf/NAMES_"${k}${i}".dbf
         fi
     done
 done
 filename=https://www.cbr.ru/vfs/credit/forms/101-20260101.rar
 wget "$filename" -O file.rar
-unar -D -f file.rar *B1* -o data
+unar -D -f file.rar *B*.* -o data
 rm file.rar
