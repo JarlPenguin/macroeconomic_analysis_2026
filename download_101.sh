6eaf413c1be57ec7888c70f04cccf516c97a1750
Jarl-Penguin <jarlpenguin@outlook.com>
2026-04-16 01:06:57 +0300

    Add more missing dbf files and update script
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>

--- download_101.sh
+++ download_101.sh
@@ -1,32 +1,20 @@
 #!/bin/bash
 
-for ((i=2004; i<=2025; i++)); do
-    for ((j=1; j<=10; j+=3)); do
+for ((i=2013; i<=2025; i++)); do
+    for ((j=1; j<=12; j++)); do
         if [ $j -lt 10 ]; then
             k=0"$j"
-        elif [ $j == 13 ]; then
-            k=01
         else
             k=$j
         fi
-        if [ $i -lt 2009 ]; then
-            ext="zip"
-        else
-            ext="rar"
-        fi
-        filename=https://www.cbr.ru/vfs/credit/forms/101-"${i}${k}"01."${ext}"
+        filename=https://www.cbr.ru/vfs/credit/forms/101-"${i}${k}"01.rar
         wget "$filename" -O file.rar
-        if [ $i -lt 2009 ]; then
-            unzip -o file.rar *B*.* -d dbf
-            unzip -p file.rar *NAMES* > dbf/NAMES_"${k}${i}".dbf
-        else
-            unar -D -f file.rar *B*.* -o dbf
-            unar -D -f file.rar -o - *NAMES* > dbf/NAMES_"${k}${i}".dbf
-        fi
+        unar -D -f file.rar *B1*.* -o dbf
+        unar -D -f file.rar -o - *NAMES* > dbf/NAMES_"${k}${i}".dbf
     done
 done
 filename=https://www.cbr.ru/vfs/credit/forms/101-20260101.rar
 wget "$filename" -O file.rar
-unar -D -f file.rar *B*.* -o dbf
+unar -D -f file.rar *B1*.* -o dbf
 unar -D -f file.rar -o - *NAMES* > dbf/NAMES_012026.dbf
 rm file.rar
